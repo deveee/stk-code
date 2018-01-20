ea2d07da477a07518b21650c77a2320d4305e02d
Deve <deveee@gmail.com>
2018-01-20 23:12:58 +0100

    Disable tutorial in help screen

--- src/states_screens/help_screen_1.cpp
+++ src/states_screens/help_screen_1.cpp
@@ -119,10 +119,6 @@ void HelpScreen1::init()
 {
     Screen::init();
     RibbonWidget* w = this->getWidget<RibbonWidget>("category");
-    ButtonWidget* tutorial = getWidget<ButtonWidget>("startTutorial");
-
-    tutorial->setActive(StateManager::get()->getGameState() !=
-                                                       GUIEngine::INGAME_MENU);
 
     if (w != NULL)  w->select( "page1", PLAYER_ID_GAME_MASTER );
 }   //init
